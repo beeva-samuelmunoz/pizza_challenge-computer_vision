bc3411c3968e32250a072067deaad5aadefea9bc
Enrique Otero <enrique.otero@beeva.com>
2018-05-25 13:35:57 +0200

    Fix variable names

--- PC_CV/config.py
+++ PC_CV/config.py
@@ -11,8 +11,8 @@ BOTTLE_MAX_BYTES_BODY = 10*(2**20)  # 10MB max file
 # API secrets not in commited files! Use config_local.py
 GOOGLE_KEY = "replace with Google access token"
 AZURE_KEY =  "replace with key"
-AWS_KEY =    "replace with AWS access key id"
-AWS_KEY_ID = "replace with AWS secret access key"
+AWS_KEY_ID = "replace with AWS access key id"
+AWS_KEY = "replace with AWS secret access key"
 
 
 try:
