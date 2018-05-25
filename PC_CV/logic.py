d4e922d30e4ac2497fdd17f3e4cea2176e988214
Samuel M.H <samuel.munoz@beeva.com>
2018-05-25 12:50:16 +0200

    OCR included, can read

--- PC_CV/logic.py
+++ PC_CV/logic.py
@@ -19,36 +19,41 @@ def crop_and_transpose_face(img, rect):
     img.paste(ic, box)
     return img
 
-def my_logic(imgbytes, az_face_client, g_annotation_client):
+def my_logic(imgbytes, g_client, az_client, aws_client):
     """Give an image, some annotations and return what you need in the template
     """
 
-    # Debug, write to disk
-    filename = '/tmp/{}.png'.format(uuid.uuid1())
-    print(filename)
-    with open(filename, 'wb') as f:
-        f.write(imgbytes)
+    imgpil = Image.open(io.BytesIO(imgbytes))  # Pillow library
 
     #TODO: your logic (modularize it to easy debug)
 
-    imgpil = Image.open(io.BytesIO(imgbytes))  # Pillow library
-
-    faces_raw = az_face_client.face_detect(filename)
+    # Detect faces
+    img_path = '/tmp/{}.png'.format(uuid.uuid1())
+    imgpil.save(img_path,'PNG')  # TODO: ñapa
+    faces_raw = az_client.face_detect(img_path)
+    os.remove(img_path)  # No longer needed
 
     if (faces_raw and faces_raw[0]['faceRectangle']):
         topic = "face"
         rect = faces_raw[0]['faceRectangle']
         imgpil = crop_and_transpose_face(imgpil, rect)
         #ic = ic.filter(ImageFilter.BLUR)
-    else:
-        tags_raw = g_annotation_client.annotate(imgbytes)
+    else:  # If no faces...what can I see?
+        tags_raw = g_client.annotate(imgbytes)
         topic = tags_raw[0].description if tags_raw else "nothing"
         imgpil = imgpil.convert("L")  # image to grayscale
+    # What can I read?
+    ret = aws_client.detect_text(imgbytes)['TextDetections']
+    ocr_lines = [
+        element['DetectedText']
+        for element in
+        ret
+        if element['Type']=='LINE'
+    ]
+
 
     # Pillow image to PNG bytearray
     imgbuffer = io.BytesIO()
     imgpil.save(imgbuffer, 'PNG')
     imgpng = imgbuffer.getvalue()
-
-    os.remove(filename)
-    return (imgpng, topic)
+    return (imgpng, topic, ocr_lines)
--- PC_CV/views/result.tpl
+++ PC_CV/views/result.tpl
@@ -6,10 +6,28 @@
 
 <body>
 <h1>Pizza Challenge - Computer Vision</h1>
-<h2>I see a {{ topic }}!</h2>
 
+<!-- Tell me what you see!! -->
+% if topic=="nothing":
+<h2>I can't see anything. I'm blind!</h2>
+% end
+% if topic!="nothing":
+<h2>I see a {{ topic }}</h2>
+% end
+
+<!-- Can you read anything? -->
+% if ocr_lines:
+<h2>I can read:</h2>
+<ul>
+  % for line in ocr_lines:
+  <li>{{ line }}</li>
+  % end
+</ul>
+% end
+
+<!-- Show me the modified image -->
 <h3>This is your processed image</h3>
-<img src="{{ image_grayscale }}" />
+<img src="{{ image }}" />
 <button id="btn_submit" onclick="window.location.href='/'">Back home</button>
 </body>
 </html>
